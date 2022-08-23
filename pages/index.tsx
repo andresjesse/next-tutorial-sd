4a59178a98c7e86c7ac3b0c4450ed10805ac8d7e
Andres <andres@pop-os.localdomain>
2022-08-23 14:35:04 -0300

    updated home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,11 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div className={styles.container}>Hello SD 2!</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Hello World - SD!</h1>
+    </div>
+  );
 };
 
 export default Home;
