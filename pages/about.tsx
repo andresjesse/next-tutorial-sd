1ace11ed4268c743d8671f034bbc4a696a8c7070
andresjesse <andres.porfirio@gmail.com>
2022-08-29 15:20:54 -0300

    rev for lesson recording

--- pages/about.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function About() {
-  return <div>SD App - 2022</div>;
-}
--- pages/computers/windows.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Windows() {
-  return <div>Windows</div>;
-}
